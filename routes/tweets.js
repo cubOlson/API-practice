13110c5e0a22197ddb5a05195335c50cca7ce8ad
Mark Gregory <markgregory19@gmail.com>
2021-03-03 14:46:18 -0800

    finished phase 3?

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,9 +1,62 @@
 const express = require('express');
+const db = require("../db/models");
+const { Tweet } = db;
 const router = express.Router();
+const { asyncHandler } = require('./utils.js');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    res.json({message: "test tweets index"});
-});
+const tweetNotFoundError = function (tweetId){
+    const err = new Error('Tweet not found');
+    err.status = 404;
+    return err
+};
+
+const tweetValidators = [
+    check('message')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 280 })
+];
+
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+      const errors = validationErrors.array().map((error) => error.msg);
+
+      const err = Error("Bad request.");
+      err.errors = errors;
+      err.status = 400;
+      err.title = "Bad request.";
+      return next(err);
+    }
+    next();
+  };
+
+router.get('/', asyncHandler( async (req, res) => {
+    const tweets = await Tweet.findAll()
+
+    res.json({ tweets });
+}));
+
+router.get('/:id(\\d+)', asyncHandler( async (req, res, next) => {
+    const tweetId = req.params.id;
+    const tweet = await Tweet.findByPk(tweetId);
+    if (tweet) {
+        res.json({ tweet })
+    } else {
+        next(tweetNotFoundError(tweetId))
+    }
+}));
+
+router.post('/', tweetValidators, handleValidationErrors, asyncHandler(
+    async (req, res, next) => {
+        const { message } = req.body
+        const tweet = await Tweet.create({
+            message
+        })
+        res.redirect('/tweets');
+    }
+));
 
 
 
---
+++ routes/utils.js
@@ -0,0 +1,8 @@
+
+const asyncHandler = (handler) => {
+    return (req, res, next) => {
+      return handler(req, res, next).catch(next);
+    }
+  };
+
+  module.exports = { asyncHandler }
