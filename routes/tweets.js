46acfcdbd142c2b5426b28a79b537f33f21452d7
Mark Gregory <markgregory19@gmail.com>
2021-03-04 10:52:18 -0800

    finished phase 3

--- routes/tweets.js
+++ routes/tweets.js
@@ -58,6 +58,31 @@ router.post('/', tweetValidators, handleValidationErrors, asyncHandler(
     }
 ));
 
+router.put('/:id(\\d+)', tweetValidators, handleValidationErrors, asyncHandler( async (req, res, next) => {
+    const tweetId = req.params.id;
+    const tweet = await Tweet.findByPk(tweetId);
+    if (tweet) {
+        const updatedTweet = await tweet.update({
+            message: req.body.message
+        })
+        res.json({ updatedTweet })
+    } else {
+        next(tweetNotFoundError(tweetId))
+    }
+    }
+));
+
+router.delete('/:id(\\d+)', handleValidationErrors, asyncHandler( async (req, res, next) => {
+    const tweetId = req.params.id;
+    const tweet = await Tweet.findByPk(tweetId);
+    if (tweet) {
+        await tweet.destroy();
+        res.status(204).end();
+    } else {
+        next(tweetNotFoundError(tweetId))
+    }
+}));
+
 
 
 module.exports = router;
